e089bf27efc6c19a33fe955f89ec42d639cbdbcc
root <root@ella.www.ella.net>
2018-10-15 13:31:45 -0600

    Commit message

--- models/user.js
+++ models/user.js
@@ -1,41 +1,20 @@
-/**
-* Sencillo script con nodejs trabajando con sqlite3,
-* bootstrap de twitter y el motor de plantillas jade
-*/
+// Modelo Usuario para la base de datos
 
-var sqlite3 = require('sqlite3').verbose(),//necesario para utilizar sqlite3
-db = new sqlite3.Database('blogNode'),//creamos la base de datos llamada blogNode si no existe
-UserModel = {};//objeto para exportar y manejar la información del modelo
+// Mongoose es una libreria de Node que nos permite
+// conectarnos a una base de datos MongoDB y modelar un esquema
+// para ella.
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-//hacemos login al usuario si existe en la tabla usuarios
-UserModel.loginUser = function(userData, callback)
-{
-	var response = {};//respuesta para devolver 
-	//consultamos si existe el usuario y sus credenciales son correctas, así escapamos los datos
-	stmt = db.prepare("SELECT * FROM usuarios WHERE username = ? AND password = ?");
-	//pasamos el nombre del usuario y el password a la consulta
-    stmt.bind(userData.username, userData.password); 
-    //usamos get para obtener una fila, así podemos devolver los datos del usuario
-    stmt.get(function(error, row) 
-    {
-        if(error) 
-        {
-            throw err;
-        } 
-        else 
-        {
-            if(row) 
-            {
-                callback({msg:"logueado",data:row});
-            }
-            else
-            {
-            	callback({msg:"error",data:""});
-            }
-        }
-    });
-}
+// Campos que vamos a guardar en la base de datos
+var UserSchema = new Schema({
+	name				: String, // Nombre del usuario
+	//provider		: String, // Cuenta del usuario (Twitter o Facebook en este ejemplo)
+	provider_id : {type: String, unique: true}, // ID que proporciona Twitter o Facebook
+	photo			 : String, // Avatar o foto del usuario
+	createdAt	 : {type: Date, default: Date.now} // Fecha de creación
+});
 
-
-//debemos escribir esta linea para poder utilizar el modelo
-module.exports = UserModel;
+// Exportamos el modelo 'User' para usarlo en otras
+// partes de la aplicación
+var User = mongoose.model('User', UserSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,77 +1,13 @@
-//llamamos al modelo user.js para utilizar toda su funcionalidad
-var UserModel = require('../models/user');
+// Rutas de la aplicación
 
-//aquí tenemos el objeto app que hemos traído de app.js
-module.exports = function(app)
-{
-
-    //para acceder aquí escribiremos http://localhost:3000/
-    //req es un objeto que contiene información sobre la solicitud HTTP que provocó el evento. 
-    //En respuesta a req, res se utiliza para devolver la respuesta HTTP que necesitemos.
-    app.get("/", function(req, res)
-    {
-        //si ha iniciado sesión no puede volver al login
-        if(req.session.username)
-        { 
-            res.redirect("/home");
-        }
-        //en este caso le decimos que queremos renderizar la vista views/index.jade con algunos datos
-        res.render('index', { 
-            title: 'Formularios en node con Jade y Twitter Bootstrap'
-        });
-    });
-
-   	//recibimos la interaccion de cuando el usuario envia el formulario de login
-    app.post("/login", function(req,res)
-    {
-    	UserModel.loginUser({username:req.body.username,password:req.body.password}, function(data)
-    	{
-    		if(data)
-    		{
-	    		//si los datos no son correctos mandamos error como respuesta
-                //a ajax,´así sabe que no ha sido correcto el login
-	    		if(data.msg === "error")
-	    		{
-	    			res.send("error", 200);
-	    		}
-                //en otro caso creamos la sesión y mandamos logueado,
-                //con lo que ajax redirigira a la home al usuario con 
-                //la sesión iniciada 
-	    		else
-	    		{
-	    			req.session.username = req.body.username;
-	    			res.send("logueado", 200);
-	    		}
-	    	}
-	    	else
-	    	{
-	    		res.send("error", 200);
-	    	}
-    	});
-    });
-
-    //mostramos la vista views/home.jade solo si el usuario ha iniciado sesion
-    app.get("/home", function(req, res)
-    {
-        //si no existe la sesion del usuario redirigimos al login
-        if(!req.session.username)
-        { 
-            res.redirect("/");
-        }
-        //en otro caso mostramos el formulario
-        else 
-        {
-            res.render('home', { 
-                title: 'Bienvenido a nuestra aplicación',
-                username: req.session.username//asi accedemos a la sesion del usuario
-            });
-        }
-    });
-
-    //cerramos la sesión del usuario y redirigimos al formulario de login
-    app.get("/logout", function(req,res)
-    {
-    	req.session.destroy();
-    	res.redirect("/");
-    })
-}
+exports.index = function(req, res){
+  // Renderiza la plantilla 'index' cuando en el navegador
+  // nos encontremos en la raiz '/' --> http://localhost:puerto/
+  res.render('index', {
+    // Enviamos como variables un título
+    // y objeto 'user' que contiene toda
+    // la información del usuario y viaja en el 'request'
+    title: 'Ejemplo de Passport JS',
+    user: req.user
+  });
+};
